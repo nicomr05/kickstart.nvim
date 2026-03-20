0caae759e1c72d9b4d3cde857298f5a70ea2da5a
nicomr <nicolasmuniz2005@gmail.com>
2026-03-21 00:40:08 +0100

    feat: hlsearch on

--- lua/custom/plugins/options.lua
+++ lua/custom/plugins/options.lua
@@ -29,7 +29,7 @@ vim.opt.autoindent = true -- Copy indent from current line
 -- Search settings
 vim.opt.ignorecase = true -- Case insensitive search
 vim.opt.smartcase = true -- Case sensitive if uppercase in search
-vim.opt.hlsearch = false -- Don"t highlight search results
+vim.opt.hlsearch = true -- Don't highlight search results
 vim.opt.incsearch = true -- Show matches as you type
 
 -- Visual settings
